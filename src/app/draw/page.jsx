137d49a6ff048c6f95d39dbbda1f6501365fbd33
akram <samirounis2@gmail.com>
2025-08-08 14:30:51 +0100

    fix the image var and change the backend endpoint for testing

--- src/app/draw/page.jsx
+++ src/app/draw/page.jsx
@@ -137,14 +137,14 @@ export default function DrawPage() {
 
     try {
       const res = await fetch(
-        "https://backend-project-u467.onrender.com/predict",
+        "http://localhost:5000/predict",
         {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
           },
           body: JSON.stringify({
-            image: imageDataURL,
+            image: imagedataurl,
           }),
         },
       );
