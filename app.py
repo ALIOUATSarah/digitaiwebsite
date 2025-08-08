b2dce79ac12620fbf56d271be3feeca6d811eb1e
zaki <z.goumri@esi-sba.dz>
2025-08-08 10:36:41 +0100

    fix(deployement):add render.yml

--- app.py
+++ app.py
@@ -11,6 +11,12 @@ CORS(app)
 
 model = tf.keras.models.load_model("cnn_mnist_model.h5")
 
+
+@app.route("/healthcheck", methods=["GET"])
+def check():
+    return jsonify({"status": "good"}), 200
+
+
 @app.route("/predict", methods=["POST"])
 def predict():
     data = request.get_json()
@@ -18,16 +24,20 @@ def predict():
 
     if not image_data:
         return jsonify({"error": "No image data provided"}), 400
-
     image_data = image_data.split(",")[1]
-    image = Image.open(BytesIO(base64.b64decode(image_data))).convert("L").resize((28, 28))
+    image = (
+        Image.open(BytesIO(base64.b64decode(image_data))).convert("L").resize((28, 28))
+    )
     image = np.array(image).reshape(1, 28, 28, 1) / 255.0
 
     prediction = model.predict(image)
     predicted_digit = np.argmax(prediction)
     confidence = float(np.max(prediction)) * 100
 
-    return jsonify({"prediction": int(predicted_digit), "confidence": round(confidence, 2)})
+    return jsonify(
+        {"prediction": int(predicted_digit), "confidence": round(confidence, 2)}
+    )
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
